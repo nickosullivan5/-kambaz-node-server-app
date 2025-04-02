e9d0c92d920c1eb535b97dbc240ae98f0e1fba53
nos <osullivan.nick@icloud.com>
2025-04-02 18:10:57 -0400

    onto 5.2.4.4

--- Lab5/index.js
+++ Lab5/index.js
@@ -2,6 +2,7 @@ import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 import Module from "./module.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
@@ -10,4 +11,5 @@ export default function Lab5(app) {
   QueryParameters(app);
   WorkingWithObjects(app);
   Module(app);
+  WorkingWithArrays(app);
 }
