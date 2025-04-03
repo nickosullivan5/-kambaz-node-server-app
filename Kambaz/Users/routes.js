53c5d775c8094113c00b469f9de30da188c45621
nos <osullivan.nick@icloud.com>
2025-04-02 21:18:44 -0400

    onto 5.3.4

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,6 +1,5 @@
 import * as dao from "./dao.js";
 
-let currentUser = null;
 export default function UserRoutes(app) {
     const createUser = (req, res) => {
     };
@@ -10,39 +9,54 @@ export default function UserRoutes(app) {
     };
     const findUserById = (req, res) => {
     };
-  const updateUser = (req, res) => {
-    const userId = req.params.userId;
-    const userUpdates = req.body;
-    dao.updateUser(userId, userUpdates);
-    currentUser = dao.findUserById(userId);
-    res.json(currentUser);
-  };
+    const updateUser = (req, res) => {
+        const userId = req.params.userId;
+        const userUpdates = req.body;
+        dao.updateUser(userId, userUpdates);
+        const currentUser = dao.findUserById(userId);
+        req.session["currentUser"] = currentUser;
+        res.json(currentUser);
+    };
 
 
     const signup = (req, res) => {
         const user = dao.findUserByUsername(req.body.username);
         if (user) {
-            res.status(400).json(
-                {message: "Username already in use"});
+            res.status(400).json({message: "Username already taken"});
             return;
         }
-        currentUser = dao.createUser(req.body);
+        const currentUser = dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
+
     const signin = (req, res) => {
         const {username, password} = req.body;
-        currentUser = dao.findUserByCredentials(username, password);
+        const currentUser = dao.findUserByCredentials(username, password);
+        if (currentUser) {
+            req.session["currentUser"] = currentUser;
+            res.json(currentUser);
+        } else {
+            res.status(401).json({message: "Unable to login. Try again later."});
+        }
+    };
+
+
+    const signout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    };
+
+
+    const profile = (req, res) => {
+        const currentUser = req.session["currentUser"];
+        if (!currentUser) {
+            res.sendStatus(401);
+            return;
+        }
         res.json(currentUser);
     };
 
-      const signout = (req, res) => {
-    currentUser = null;
-    res.sendStatus(200);
-  };
-
-      const profile = async (req, res) => {
-    res.json(currentUser);
-  };
 
     app.post("/api/users", createUser);
     app.get("/api/users", findAllUsers);
