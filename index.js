f3ca8c1bfeebf0da9313e653fea2e77205878d00
nos <osullivan.nick@icloud.com>
2025-04-09 10:08:04 -0400

    moving onto 6.2.6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-// import Hello from "./Hello.js"
+import mongoose from "mongoose";
+
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
@@ -8,7 +9,8 @@ import "dotenv/config";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
-
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
  cors({
